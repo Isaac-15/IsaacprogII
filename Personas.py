7e911051b19685edbc473586615071c5b6e200b3
Isaac <isaaccarmona281@gmail.com>
2024-02-27 15:56:09 -0500

    Personas Python

--- Personas.py
+++ Personas.py
@@ -8,10 +8,8 @@ class Persona:
             self.correo = correo
             self.telefono = telefono
 
-    def ObtenerInfo(self):
-        return f"Mi nombre es: {self.nombre}\n Mi apellido es: {self.apellido}\n Mi cedula es: {self.cedula}\n Mi correo es: {self.correo}\n Mi telefono es: {self.telefono}"
-
+    
 P= Persona("Isaac","Carmona"," 1043967918","isaaccarmona281@gmail.com"," 301619461")
 
-print(P.ObtenerInfo())
+print("mi nombre es "+P.nombre," mi apellido es "+P.apellido," mi cedula es "+P.cedula," mi correo es "+P.correo," y mi telefono "+P.telefono)
 
