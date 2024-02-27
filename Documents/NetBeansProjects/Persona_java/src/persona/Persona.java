d9d3df5e44d6402c7ef68733397db82f6235b1bd
Isaac <isaaccarmona281@gmail.com>
2024-02-27 16:01:22 -0500

    Personas java

---
+++ Documents/NetBeansProjects/Persona_java/src/persona/Persona.java
@@ -0,0 +1,24 @@
+
+package persona;
+
+
+public class Persona {
+
+  
+    public static void main(String[] args) {
+        
+        Personas P = new Personas();
+        
+        P.setNombre("isaac");
+        P.setCedula(1043967918);
+        P.setTelefono("3016191461");
+        P.setCorreo("isaaccarmona281@gmail.com");
+        
+        
+        System.out.println("mi nombre es "+ P.getNombre()); System.out.println("mi cedula "+ P.cedula); System.out.println("por si me quieres llamar "+ P.telefono); System.out.println("mi correo "+ P.correo);
+        
+    }
+    
+   
+    
+}
